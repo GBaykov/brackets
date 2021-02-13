150d04fa16080712b4f8147430dcd6d7895c6b7b
GBaykov <73140481+GBaykov@users.noreply.github.com>
2021-02-13 14:33:56 +0300

    fix: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let sum = '';
+    let count = 0;
+   while (str !== '') {
+     count = 0;
+      for (let sub of bracketsConfig ){
+      sum = sub[0] + sub[1]
+        if (str.includes(sum)) {
+      str = str.replace(sum, '');
+      count = count+1;
+      }
+    } if (count === 0) break;
+   } if (str === '') return true;
+   else return false;
 }
